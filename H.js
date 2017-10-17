d993c1c3a7dc7095a37b253c8cb16ec6ac14c42c
ylon <openlemon@gmail.com>
2017-10-17 13:11:47 +0200

    first

---
+++ H.js
@@ -0,0 +1,23 @@
+var filesizeParser = require('filesize-parser');
+
+H = {}
+module.exports = H;
+
+// human to bytes
+H.filesize_parse = (string) => {
+  return filesizeParser(string)
+}
+
+/*
+size: number of bytes | human string size
+return int
+*/
+H.filesize_MB = (size) => {
+  if (typeof size == 'string') size = H.filesize_parse(size)
+  return Math.floor(size / 1024 / 1024)
+}
+
+H.re_firstMatch = (data, re) => {
+  var m = re.exec(data);
+  return m != null && m.length > 0 ? m[1] : null;
+}
