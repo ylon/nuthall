62af5d165cf0a8d28f792efbf75e5ee159158bc2
ylon <openlemon@gmail.com>
2019-03-23 09:31:48 +0100

    massive nuthall

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports = {
-  yggtorrent: (...args) => new (require('./src/Yggtorrent'))(...args),
-  yggtorrentClass: (...args) => require('./src/Yggtorrent')
+  rarbg: (...args) => new (require('./lib/sources/Rarbg'))(...args),
+  Torrent: require('./lib/Torrent'),
+  torrentSearchApi: () => require('torrent-search-api'),
+  Transmission: require('./lib/Transmission'),
+  yggtorrent: (...args) => new (require('./lib/sources/Yggtorrent'))(...args),
+  Yggtorrent: require('./lib/sources/Yggtorrent')
 };
---
+++ wiki/notes.md
@@ -0,0 +1,3 @@
+"torrent-search-api": "git+https://github.com/ylon/torrent-search-api.git#nuthall"
+
+nuthall searchSerie --source yggtorrent --string 'game of' --format object|json --file 'result'
